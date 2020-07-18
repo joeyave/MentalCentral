f70fd1e67f45ce014da00f1775fcfbffe8d42511
Alex2001Shurik <57028313+Alex2001Shurik@users.noreply.github.com>
2020-07-18 12:07:24 +0300

    pattern for number

--- src/main/java/com/noosphere/domain/Patient.java
+++ src/main/java/com/noosphere/domain/Patient.java
@@ -39,6 +39,7 @@ public class Patient implements Serializable {
     private String address;
 
     @NotNull
+    @Pattern(regexp = "+380[0-9]{9}")
     @Column(name = "phone", nullable = false)
     private String phone;
 
